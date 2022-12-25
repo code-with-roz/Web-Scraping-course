046718135a5e4169beed815f38eae0ae5fbbfdb2
Rozina Myoya <code.with.roz@gmail.com>
2022-12-25 14:40:19 +0200

    scraping chat content from blog

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -4,12 +4,13 @@ import scrapy
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
     allowed_domains = ['jamiiforums.com']
-    start_urls = ['http://jamiiforums.com/']
+    start_urls = ['https://www.jamiiforums.com/threads/usafiri-mtandaoni.2047350/']
 
     def parse(self, response):
         #title = response.css('span.title::text').get()
-        title = response.xpath('//span[@class="title"]/text()').get()
+        #title = response.xpath('//title/text()').get()
+        content = response.xpath('//div[@class="bbWrapper"]/text()').getall()
         
         
         #title = response.xpath()
-        return {"title": title}
+        return {"title": content}
